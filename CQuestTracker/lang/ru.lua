76202eca7c9a898572f93f2bece4b7070ad13924
jimer2121 <40351897+jimer2121@users.noreply.github.com>
2022-01-08 15:32:10 +0300

    Add files via upload
    Ru  loc

---
+++ CQuestTracker/lang/ru.lua
@@ -0,0 +1,52 @@
+------------------------------------------------
+-- CQuestTracker
+-- Russian localization
+-- Version 1, 08-01-2022, by Inzenieris#2546
+------------------------------------------------
+
+--	Localization Strings
+SafeAddString(SI_CQT_WELCOME_TEXT1, 							" Добро пожаловать в аддон CQuestTracker!", 1)
+SafeAddString(SI_CQT_WELCOME_TEXT2, 							" Этот аддон автоматически обновляет лист отображаемых квестов и показывает последние из них с большим приоритетом.", 1)
+SafeAddString(SI_CQT_WELCOME_TEXT3, 							" Изначально, отображается только отслеживаемый квест. Если вы хотите отображать дополнительные квесты сразу, сделайте следующее: ", 1)
+SafeAddString(SI_CQT_WELCOME_TEXT4, 							" Откройте список квестов в левом верхнем углу окна трекера и выберите нужный квест, щелкнув левой кнопкой мыши по названию квеста. Вы также можете закрепить определенный квест, чтобы показывать его все время, или игнорировать, чтобы скрыть.", 1)
+SafeAddString(SI_CQT_TITLEBAR_QUEST_LIST_BUTTON_TIPS, 			"Список квестов", 1)
+SafeAddString(SI_CQT_TITLEBAR_OPEN_SETTINGS_BUTTON_TIPS, 		"Открыть настройки", 1)
+SafeAddString(SI_CQT_QUESTLIST_MENU_HEADER, 					"Список квестов: (<<1>>/<<2>>)", 1)
+SafeAddString(SI_CQT_ENABLE_PINNING_QUEST, 						"Закрепить квест", 1)
+SafeAddString(SI_CQT_DISABLE_PINNING_QUEST, 					"Открепить квест", 1)
+SafeAddString(SI_CQT_ENABLE_IGNORING_QUEST, 					"Игнорировать квест", 1)
+SafeAddString(SI_CQT_DISABLE_IGNORING_QUEST, 					"Не игнорировать квест", 1)
+SafeAddString(SI_CQT_PICK_OUT_QUEST, 							"Выбрать квест", 1)
+SafeAddString(SI_CQT_RULE_OUT_QUEST, 							"Убрать квест", 1)
+SafeAddString(SI_CQT_MOST_LOWER_QUEST, 							"Вывести квест в низ списка", 1)  --not shure
+
+SafeAddString(SI_CQT_QUESTTYPE_ZONE_STORY_QUEST, 				"Сюжетный квест локации", 1)
+SafeAddString(SI_CQT_QUESTTYPE_SIDE_QUEST, 						"Побочный  квест", 1)
+SafeAddString(SI_CQT_QUEST_BACKGROUND_HEADER, 					"Фон:", 1)
+SafeAddString(SI_CQT_QUEST_OR_DESCRIPTION, 						L(SI_QUEST_OR_DESCRIPTION), 1)
+SafeAddString(SI_CQT_QUEST_OPTIONAL_STEPS_DESCRIPTION, 			L(SI_QUEST_OPTIONAL_STEPS_DESCRIPTION), 1)
+SafeAddString(SI_CQT_QUEST_OPTIONAL_STEPS_OR_DESCRIPTION, 		"Опциональные шаги  - выполните один из:", 1)
+SafeAddString(SI_CQT_QUEST_OBJECTIVES_HEADER, 					"<<C:1[Нет примечаний/Примечание/Примечания]>>:", 1)
+SafeAddString(SI_CQT_QUEST_OBJECTIVES_OR_HEADER, 				"<<C:1[Нет примечаний/Примечание/Примечания - выполните один из]>>:", 1)
+SafeAddString(SI_CQT_QUEST_HINT_STEPS_HEADER, 					"<<C:1[Нет примечаний/Примечание/Примечания]>>:", 1)
+
+SafeAddString(SI_CQT_UI_PANEL_HEADER1_TEXT, 					"Этоn аддон отслеживет несколько квестов, основываясь на ваших последних действиях, связанных с квестами..", 1)
+SafeAddString(SI_CQT_UI_ACCOUNT_WIDE_OP_NAME, 					"Настройки на аккаунт", 1)
+SafeAddString(SI_CQT_UI_ACCOUNT_WIDE_OP_TIPS, 					"Если параметр Настройки на аккаунт отключен, то у каждого персонажа могут быть разные параметры конфигурации, установленные ниже.", 1)
+SafeAddString(SI_CQT_UI_BEHAVIOR_HEADER1_TEXT, 					"Настройки  поведения", 1)
+SafeAddString(SI_CQT_UI_HIDE_DEFAULT_TRACKER_OP_NAME, 			"Спрятать стандартное отслеживание  квестов", 1)
+SafeAddString(SI_CQT_UI_HIDE_DEFAULT_TRACKER_OP_TIPS, 			"Спрятать стандартное отслеживание  квестов", 1)
+SafeAddString(SI_CQT_UI_HIDE_QUEST_TRACKER_OP_NAME, 			"Спрятать аддон", 1)
+SafeAddString(SI_CQT_UI_HIDE_QUEST_TRACKER_OP_TIPS, 			"Спрятать аддон", 1)
+SafeAddString(SI_CQT_UI_TRACKER_VISUAL_HEADER1_TEXT,			"Настройки  отображения", 1)
+SafeAddString(SI_CQT_UI_QUEST_NAME_FONT_SUBHEADER_TEXT, 		"Шрифт имени квеста:", 1)
+SafeAddString(SI_CQT_UI_QUEST_CONDITION_FONT_SUBHEADER_TEXT, 	"Шрифт задач квеста:", 1)
+SafeAddString(SI_CQT_UI_COMMON_FONTTYPE_MENU_NAME,  			"|u25:0::|uШрифт", 1)
+SafeAddString(SI_CQT_UI_COMMON_FONTSIZE_MENU_NAME,  			"|u25:0::|uРазмер шрифта", 1)
+SafeAddString(SI_CQT_UI_COMMON_FONTWEIGHT_MENU_NAME,  			"|u25:0::|uВес шрифта", 1)
+SafeAddString(SI_CQT_UI_QUEST_NAME_FONTTYPE_MENU_TIPS,  		"Выбор предпочитаемого шрифта.", 1)
+SafeAddString(SI_CQT_UI_QUEST_NAME_FONTSIZE_MENU_TIPS,  		"Выбор предпочитаемого размера шрифта.", 1)
+SafeAddString(SI_CQT_UI_QUEST_NAME_FONTWEIGHT_MENU_TIPS,  		"Выбор предпочитаемого веса шрифта.", 1)
+SafeAddString(SI_CQT_UI_QUEST_CONDITION_FONTTYPE_MENU_TIPS,  	"Выбор предпочитаемого шрифта.", 1)
+SafeAddString(SI_CQT_UI_QUEST_CONDITION_FONTSIZE_MENU_TIPS,  	"Выбор предпочитаемого размера шрифта.", 1)
+SafeAddString(SI_CQT_UI_QUEST_CONDITION_FONTWEIGHT_MENU_TIPS, 	"Выбор предпочитаемого веса шрифта.", 1)
